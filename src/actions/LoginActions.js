8727d5e9740ae2308a633330f4a72452d3840bf4
loehnertz <jakob2102@gmail.com>
2017-03-18 00:47:04 +0100

    Added the actual 'login' logic to the 'LoginActions'

--- src/actions/LoginActions.js
+++ src/actions/LoginActions.js
@@ -19,3 +19,29 @@ export const passwordChanged = (text) => {
         payload: text
     };
 };
+
+export const loginUser = (user, password) => {
+    return (dispatch) => {
+        dispatch({ type: LOADING_START });
+
+        fetch('https://hsrm-medialab.de/osp/server/functions.php', {
+            method: 'POST',
+            body: `request=login&timestamp=${Date.now()}&editor=anonymous&user=${user}&password=${password}`,
+            headers: {
+                'Connection': 'keep-alive',
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+            }
+        })
+            .then((response) => response.json())
+            .then((responseJson) => {
+                if (responseJson.message === "login successful") {
+                    dispatch({ type: LOGIN_USER_SUCCESS });
+                } else {
+                    dispatch({ type: LOGIN_USER_FAILED });
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+};
