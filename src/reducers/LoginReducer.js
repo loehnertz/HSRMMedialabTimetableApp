1fdbdf103959bdcaebf20fb7009b1fbafe26941a
loehnertz <jakob2102@gmail.com>
2017-03-18 02:48:09 +0100

    The reducer for the 'login' now returns the username to app-level state

--- src/reducers/LoginReducer.js
+++ src/reducers/LoginReducer.js
@@ -11,6 +11,7 @@ const INITIAL_STATE = {
     userField: '',
     passwordField: '',
     error: '',
+    user: '',
     loadingLogin: false
 };
 
@@ -21,11 +22,11 @@ export default (state = INITIAL_STATE, action) => {
         case PASSWORD_CHANGED:
             return { ...state, passwordField: action.payload };
         case LOADING_START:
-            return { ...state, loadingLogin: true, error: '' };
+            return { ...state, loadingLogin: true, error: "" };
         case LOADING_END:
             return { ...state, loadingLogin: false };
         case LOGIN_USER_SUCCESS:
-            return { ...state, error: "", userField: "", passwordField:  "", loadingLogin: false };
+            return { ...state, error: "", userField: "", passwordField:  "", user: action.payload, loadingLogin: false };
         case LOGIN_USER_FAILED:
             return { ...state, error: i18n.t('login_failed'), passwordField:  "", loadingLogin: false };
         default:
