a11b7b370ced338110285de2b98e9e1ea9091225
loehnertz <jakob2102@gmail.com>
2017-03-18 14:57:24 +0100

    Built in a helper-function to render every 'lecturer' from the array of them (and join them with commas)

--- src/components/ListItemComponent.js
+++ src/components/ListItemComponent.js
@@ -11,13 +11,31 @@ class ListItem extends Component {
         console.log("Pressed");
     }
 
+    renderLecturers() {
+        let lecturersArray = JSON.parse(this.props.eventLecturers);
+        let lecturersString = '';
+        for (let lecturer in lecturersArray) {
+            lecturersString = lecturersString + lecturersArray[lecturer];
+            if (!(lecturer + 1) == lecturersArray.length) {  // Check if it's NOT the last iteration
+                lecturersString = lecturersString + ', ';
+            }
+        }
+        return lecturersString;
+    }
+
     render() {
         return (
             <TouchableWithoutFeedback onPress={this.onRowPress.bind(this)}>
                 <View>
                     <CardSection>
                         <Text style={styles.titleStyle}>
-                            {this.props.event}
+                            {this.props.eventName}
+                        </Text>
+                        <Text style={styles.titleStyle}>
+                            {this.props.eventRoom}
+                        </Text>
+                        <Text style={styles.titleStyle}>
+                            {this.renderLecturers()}
                         </Text>
                     </CardSection>
                 </View>
