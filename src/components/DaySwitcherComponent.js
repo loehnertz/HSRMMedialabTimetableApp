4abdc6bed120d4ea1e8ff6b8d07ac548cf7b4952
loehnertz <jakob2102@gmail.com>
2017-03-18 16:35:40 +0100

    Added helper-functions to 'add' or 'subtract' from the current day to select a new one

--- src/components/DaySwitcherComponent.js
+++ src/components/DaySwitcherComponent.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
 import {
     Text,
-    View
+    View,
+    Button
 } from 'react-native';
+import { connect } from 'react-redux';
+import moment from 'moment';
+import { selectDay } from '../actions';
 
 class DaySwitcher extends Component {
+    selectNextDay(){
+        let nextDay = moment().day(this.props.selectedDay).week(this.props.currentWeek).add(1, 'day').format('ddd');
+        this.props.selectDay(nextDay);
+    }
+
+    selectPrevDay(){
+        let prevDay = moment().day(this.props.selectedDay).week(this.props.currentWeek).subtract(1, 'day').format('ddd');
+        this.props.selectDay(prevDay);
+    }
+
     render() {
         return (
             <View style={styles.bottomBar}>
+                <Button
+                    onPress={this.selectPrevDay.bind(this)}
+                    title="<"
+                    color="#841584"
+                    accessibilityLabel="Previous Day"
+                />
                 <Text>{this.props.day}</Text>
+                <Button
+                    onPress={this.selectNextDay.bind(this)}
+                    title=">"
+                    color="#841584"
+                    accessibilityLabel="Next Day"
+                />
             </View>
         );
     }
@@ -19,8 +45,18 @@ const styles = {
         position: "absolute",
         bottom: 0,
         alignSelf: "center",
-        padding: 20
+        flexDirection: "row",
+        justifyContent: "space-between",
+        padding: 20,
+        flex: 1
     }
 };
 
-export default DaySwitcher;
+const mapStateToProps = state => {
+    return {
+        currentWeek: state.timetable.currentWeek,
+        selectedDay: state.timetable.selectedDay
+    }
+};
+
+export default connect(mapStateToProps, { selectDay })(DaySwitcher);
