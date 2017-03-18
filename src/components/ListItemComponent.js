80f8764235a102d9749191a5420f504a1fc9ec2d
loehnertz <jakob2102@gmail.com>
2017-03-18 13:29:17 +0100

    Added 'ListItem' component for native list rendering

---
+++ src/components/ListItemComponent.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import {
+    Text,
+    TouchableWithoutFeedback,
+    View
+} from 'react-native';
+import { CardSection } from './common';
+
+class ListItem extends Component {
+    onRowPress() {
+        console.log("Pressed");
+    }
+
+    render() {
+        return (
+            <TouchableWithoutFeedback onPress={this.onRowPress.bind(this)}>
+                <View>
+                    <CardSection>
+                        <Text style={styles.titleStyle}>
+                            {this.props.event}
+                        </Text>
+                    </CardSection>
+                </View>
+            </TouchableWithoutFeedback>
+        );
+    }
+}
+
+const styles = {
+    titleStyle: {
+        fontSize: 18,
+        paddingLeft: 15
+    }
+};
+
+export default ListItem;
