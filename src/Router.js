fd758826e313f33666d9c013dd97f87bc3ae2777
loehnertz <jakob2102@gmail.com>
2017-03-18 02:31:40 +0100

    Added a first scene for the 'main' scene container

--- src/Router.js
+++ src/Router.js
@@ -4,6 +4,7 @@ import i18n from 'react-native-i18n';
 import bundledTranslations from './translations';
 import Startup from './components/StartupComponent';
 import LoginForm from './components/LoginFormComponent';
+import WeekView from './components/WeekView';
 
 i18n.fallbacks = true;
 i18n.translations = bundledTranslations;
@@ -12,10 +13,13 @@ const RouterComponent = () => {
     return (
         <Router sceneStyle={{ paddingTop: 60 }}>
             <Scene key="startup" inital>
-                <Scene key="loading" component={Startup} title="Loading" />
+                <Scene key="loading" component={Startup} title={i18n.t('loading')} />
             </Scene>
             <Scene key="auth">
                 <Scene key="login" component={LoginForm} title={i18n.t('login')} />
+            </Scene>
+            <Scene key="main">
+                <Scene key="weekView" component={WeekView} title={i18n.t('week_view')} />
             </Scene>
         </Router>
     );
