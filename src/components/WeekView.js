7078369f3ab9f6306326ba216d9d9f5a47864a0f
loehnertz <jakob2102@gmail.com>
2017-03-18 03:11:51 +0100

    Added 'Spinner' to indicate fetching of the week

--- src/components/WeekView.js
+++ src/components/WeekView.js
@@ -5,6 +5,7 @@ import {
 } from 'react-native';
 import { connect } from 'react-redux';
 import moment from 'moment';
+import { Spinner } from './common';
 import { fetchWeek } from '../actions';
 
 class WeekView extends Component {
@@ -12,12 +13,24 @@ class WeekView extends Component {
         this.props.fetchWeek(this.props.user, moment().format('W'));
     }
 
-    render() {
-        return (
-            <View>
+    renderContent(){
+        if (this.props.loading) {
+            return (
+                <Spinner size="small" />
+            );
+        } else {
+            return (
                 <Text>
                     {this.props.week}
                 </Text>
+            );
+        }
+    }
+
+    render() {
+        return (
+            <View style={{ paddingTop: 10 }}>
+                {this.renderContent()}
             </View>
         );
     }
@@ -26,7 +39,8 @@ class WeekView extends Component {
 const mapStateToProps = state => {
     return {
         user: state.login.user,
-        week: state.timetable.fetchedWeek
+        week: state.timetable.fetchedWeek,
+        loading: state.timetable.loadingFetch
     }
 };
 
