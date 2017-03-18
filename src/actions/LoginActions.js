c0c29cf6497deab41013ee07ede5f60a97485b8c
loehnertz <jakob2102@gmail.com>
2017-03-18 02:22:10 +0100

    Added function to check whether the credentials are saved or not and if so use them for a login attempt

--- src/actions/LoginActions.js
+++ src/actions/LoginActions.js
@@ -23,6 +23,43 @@ export const passwordChanged = (text) => {
     };
 };
 
+export const isUserLoggedIn = () => {
+    return (dispatch) => {
+        Keychain.getGenericPassword()
+            .then((credentials) => {
+                if (credentials) {
+                    fetch('https://hsrm-medialab.de/osp/server/functions.php', {
+                        method: 'POST',
+                        body: `request=login&timestamp=${Date.now()}&editor=anonymous&user=${credentials.username}&password=${credentials.password}`,
+                        headers: {
+                            'Connection': 'keep-alive',
+                            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+                        }
+                    })
+                        .then((response) => response.json())
+                        .then((responseJson) => {
+                            if (responseJson.message === "login successful") {
+                                dispatch({ type: LOGIN_USER_SUCCESS });
+                                Actions.main({ type: 'reset' });
+                            } else {
+                                dispatch({ type: LOGIN_USER_FAILED });
+                                Actions.auth({ type: 'reset' });
+                            }
+                        })
+                        .catch((error) => {
+                            console.log(error);
+                            Actions.auth({ type: 'reset' });
+                        });
+                } else {
+                    Actions.auth({ type: 'reset' });
+                }
+            })
+            .catch(() => {
+                Actions.auth({ type: 'reset' });
+            });
+    };
+};
+
 export const loginUser = (user, password) => {
     return (dispatch) => {
         dispatch({ type: LOADING_START });
