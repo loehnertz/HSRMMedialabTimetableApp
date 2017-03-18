78edc46541d9a220f3757a0dcb802e1728214fa6
loehnertz <jakob2102@gmail.com>
2017-03-18 12:23:41 +0100

    Refactored the 'Router' to handle fetching a week in a dirty way (cleaner way is planned for the future)

--- src/Router.js
+++ src/Router.js
@@ -1,28 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Scene, Router } from 'react-native-router-flux';
+import { fetchWeek } from './actions';
 import i18n from 'react-native-i18n';
 import bundledTranslations from './translations';
 import Startup from './components/StartupComponent';
 import LoginForm from './components/LoginFormComponent';
-import WeekView from './components/WeekView';
+import Timetable from './components/TimetableComponent';
 
 i18n.fallbacks = true;
 i18n.translations = bundledTranslations;
 
-const RouterComponent = () => {
-    return (
-        <Router sceneStyle={{ paddingTop: 55 }}>
-            <Scene key="startup" inital>
-                <Scene key="loading" component={Startup} title={i18n.t('loading')} />
-            </Scene>
-            <Scene key="auth">
-                <Scene key="login" component={LoginForm} title={i18n.t('login')} />
-            </Scene>
-            <Scene key="main">
-                <Scene key="weekView" component={WeekView} title={i18n.t('week_view')} />
-            </Scene>
-        </Router>
-    );
+class RouterComponent extends Component {
+    render() {
+        return (
+            <Router sceneStyle={{ paddingTop: 55 }}>
+                <Scene key="startup" inital>
+                    <Scene key="loading" component={Startup} title={i18n.t('loading')} />
+                </Scene>
+                <Scene key="auth">
+                    <Scene key="login" component={LoginForm} title={i18n.t('login')} />
+                </Scene>
+                <Scene key="main">
+                    <Scene
+                        key="timetable"
+                        component={Timetable}
+                        title={i18n.t('day_view')}
+                        leftTitle="Vorherige"
+                        onLeft={() => this.props.fetchWeek(this.props.user, (this.props.currentWeek - 1))}
+                        rightTitle="Nächste"
+                        onRight={() => this.props.fetchWeek(this.props.user, (this.props.currentWeek + 1))}
+                    />
+                </Scene>
+            </Router>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        user: state.login.user,
+        currentWeek: state.timetable.currentWeek
+    }
 };
 
-export default RouterComponent;
+export default connect(mapStateToProps, { fetchWeek })(RouterComponent);
