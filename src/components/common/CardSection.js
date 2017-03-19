1b9249a9cf808d3ee60e91a3e2da58402ae71082
Jakob Löhnertz <jakob2102@gmail.com>
2017-03-19 16:39:34 +0100

    Gave the 'CardSection' some more padding to breathe

--- src/components/common/CardSection.js
+++ src/components/common/CardSection.js
@@ -16,7 +16,7 @@ const CardSection = (props) => {
 const styles = {
     containerStyle: {
         borderBottomWidth: 1,
-        padding: 5,
+        padding: 10,
         backgroundColor: "white",
         justifyContent: "flex-start",
         flexDirection: "row",
