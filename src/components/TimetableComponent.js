38a967427e5d888e47449f5dcaa9944a334b8d0d
loehnertz <jakob2102@gmail.com>
2017-03-21 15:51:41 +0100

    Added the detection of the 'initial' as well as 'changed' orientation of the device

--- src/components/TimetableComponent.js
+++ src/components/TimetableComponent.js
@@ -8,18 +8,28 @@ import {
     UIManager
 } from 'react-native';
 import { connect } from 'react-redux';
-import { Actions } from 'react-native-router-flux';
 import moment from 'moment';
+import Orientation from 'react-native-orientation';
 import { fetchWeek, selectDay } from '../actions';
 import i18n from 'react-native-i18n';
 import bundledTranslations from '../translations';
 import DayView from './DayViewComponent';
 import DaySwitcher from './DaySwitcherComponent';
+import WeekView from './WeekViewComponent';
 
 class Timetable extends Component {
     componentWillMount() {
         setTimeout(() => { this.props.fetchWeek(this.props.user, this.props.program, '17', this.props.semester); }, 1000);  // Using the 17th week of the year to get results from the API
         // TODO: Try to refactor the line above so one does not need the 'setTimeout()'
+
+        let initialOrientation = Orientation.getInitialOrientation();
+        if (initialOrientation === 'PORTRAIT') {
+            this.setState({ orientation: initialOrientation });
+        } else if (initialOrientation === 'LANDSCAPE') {
+            this.setState({ orientation: initialOrientation });
+        }
+
+        Orientation.addOrientationListener(this._orientationDidChange);
 
         let today = moment().format('ddd');
         let hour = moment().format('H');
@@ -40,6 +50,14 @@ class Timetable extends Component {
         LayoutAnimation.easeInEaseOut();
     }
 
+    _orientationDidChange = (changedOrientation) => {
+        if (changedOrientation === 'PORTRAIT') {
+            this.setState({ orientation: changedOrientation });
+        } else if (changedOrientation === 'LANDSCAPE') {
+            this.setState({ orientation: changedOrientation });
+        }
+    };
+
     renderTimetable() {
         if (this.props.loading) {
             return (
@@ -49,27 +67,35 @@ class Timetable extends Component {
             );
         } else {
             if (this.props.week && this.props.slots) {
-                let events = [];
-                let eventsList = JSON.parse(this.props.week)["events"];
-                let eventIndex = 0;  // Introduced this variable because the index of 'let event' is forged due to an if-statement inside the loop
+                if (this.state.orientation === 'PORTRAIT') {  // Render: DayView
+                    let events = [];
+                    let eventsList = JSON.parse(this.props.week)["events"];
+                    let eventIndex = 0;  // Introduced this variable because the index of 'let event' is forged due to an if-statement inside the loop
 
-                for (let event in eventsList) {
-                    if (eventsList[event]["day"] === this.props.selectedDay) {
-                        events.push(eventsList[event]);
-                        events[eventIndex]["slot"] = `${this.props.slots[eventsList[event]["start"]].start}\n    -\n${this.props.slots[eventsList[event]["end"]].end}`;
-                        eventIndex += 1;  // Increase the actual index by one every iteration the if-statement passes
+                    for (let event in eventsList) {
+                        if (eventsList[event]["day"] === this.props.selectedDay) {
+                            events.push(eventsList[event]);
+                            events[eventIndex]["slot"] = `${this.props.slots[eventsList[event]["start"]].start}\n    -\n${this.props.slots[eventsList[event]["end"]].end}`;
+                            eventIndex += 1;  // Increase the actual index by one every iteration the if-statement passes
+                        }
                     }
+
+                    let day = moment().day(this.props.selectedDay).week(this.props.currentWeek).format('ddd');
+                    let date = moment().day(this.props.selectedDay).week(this.props.currentWeek).format('DD.MM.YYYY');
+
+                    return (
+                        <View style={styles.dayView}>
+                            <DayView events={events} />
+                            <DaySwitcher day={i18n.t(day) + ', ' + date} />
+                        </View>
+                    );
+                } else if (this.state.orientation === 'LANDSCAPE') {  // Render: WeekView
+                    return (
+                        <View>
+                            <WeekView />
+                        </View>
+                    );
                 }
-
-                let day = moment().day(this.props.selectedDay).week(this.props.currentWeek).format('ddd');
-                let date = moment().day(this.props.selectedDay).week(this.props.currentWeek).format('DD.MM.YYYY');
-
-                return (
-                    <View style={styles.dayView}>
-                        <DayView events={events} />
-                        <DaySwitcher day={i18n.t(day) + ', ' + date} />
-                    </View>
-                );
             }
         }
     }
