dcb161ba9d33b06a1b0b6d32c08a4e9570cbb18c
loehnertz <jakob2102@gmail.com>
2017-03-21 15:13:46 +0100

    Scaffolded the 'WeekView' component

---
+++ src/components/WeekViewComponent.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import {
+    Text,
+    View,
+    ListView,
+    Button,
+    RefreshControl
+} from 'react-native';
+import { connect } from 'react-redux';
+import { fetchWeek } from '../actions';
+import i18n from 'react-native-i18n';
+import bundledTranslations from '../translations';
+
+class WeekView extends Component {
+    render() {
+        return (
+            <View style={styles.flex}>
+                <Text>WeekView</Text>
+            </View>
+        );
+    }
+}
+
+i18n.locale = 'de';
+i18n.fallbacks = true;
+i18n.translations = bundledTranslations;
+
+const styles = {
+    flex: {
+        flex: 1
+    }
+};
+
+const mapStateToProps = state => {
+    return {
+        user: state.login.user,
+        program: state.login.program,
+        masterdata: state.timetable.masterdata,
+        currentWeek: state.timetable.currentWeek,
+        semester: state.settings.semester,
+        special_subject: state.settings.special_subject
+    }
+};
+
+export default connect(mapStateToProps, { fetchWeek })(WeekView);
