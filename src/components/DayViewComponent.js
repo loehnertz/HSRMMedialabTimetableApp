loehnertz/HSRMMedialabTimetableApp

commit 2b7d7051a9907405c016882f785369a2cc316363

Added check for a 'special_subject' to only display events of the set 'special_subject'

src/components/DayViewComponent.js
@@ -50,16 +50,20 @@ class DayView extends Component {
             return <Text style={styles.noEventsText}>{i18n.t('no_events')}</Text>;
         } else {
             eventName = _.find(masterdataJSON["programs"][this.props.program]["courses"], { 'course': eventJSON["course"] })["shortname"];
-            eventRoom = eventJSON["rooms"][0];
-            for (let lecturer in eventJSON["lecturers"]) {
-                eventLecturers.push(_.get(masterdataJSON["persons"], eventJSON["lecturers"][lecturer])["name"]);
-            }
-            eventLecturers = JSON.stringify(eventLecturers);
-            eventNote = eventJSON["note"];
-            eventSlot = eventJSON["slot"];
-        }
+            if (eventName.includes(this.props.special_subject === 'all' || this.props.special_subject.toUpperCase())) {
+                eventRoom = eventJSON["rooms"][0];
+                for (let lecturer in eventJSON["lecturers"]) {
+                    eventLecturers.push(_.get(masterdataJSON["persons"], eventJSON["lecturers"][lecturer])["name"]);
+                }
+                eventLecturers = JSON.stringify(eventLecturers);
+                eventNote = eventJSON["note"];
+                eventSlot = eventJSON["slot"];
 
-        return <ListItem eventName={eventName} eventRoom={eventRoom} eventLecturers={eventLecturers} eventNote={eventNote} eventSlot={eventSlot} />;
+                return <ListItem eventName={eventName} eventRoom={eventRoom} eventLecturers={eventLecturers} eventNote={eventNote} eventSlot={eventSlot} />;
+            } else {
+                return null;
+            }
+        }
     }
 
     render() {
@@ -133,7 +137,8 @@ const mapStateToProps = state => {
         user: state.login.user,
         program: state.login.program,
         currentWeek: state.timetable.currentWeek,
-        selectedDay: state.timetable.selectedDay
+        selectedDay: state.timetable.selectedDay,
+        special_subject: state.settings.special_subject
     }
 };
 

commit 5b26756832d4f960246cc37db5096c7c3963631c

Fixed a bug from commit 2b7d705 caused by a typo

src/components/DayViewComponent.js
@@ -50,7 +50,7 @@ class DayView extends Component {
             return <Text style={styles.noEventsText}>{i18n.t('no_events')}</Text>;
         } else {
             eventName = _.find(masterdataJSON["programs"][this.props.program]["courses"], { 'course': eventJSON["course"] })["shortname"];
-            if (eventName.includes(this.props.special_subject === 'all' || this.props.special_subject.toUpperCase())) {
+            if (this.props.special_subject === 'all' || eventName.includes(this.props.special_subject.toUpperCase())) {
                 eventRoom = eventJSON["rooms"][0];
                 for (let lecturer in eventJSON["lecturers"]) {
                     eventLecturers.push(_.get(masterdataJSON["persons"], eventJSON["lecturers"][lecturer])["name"]);
@@ -61,7 +61,7 @@ class DayView extends Component {
 
                 return <ListItem eventName={eventName} eventRoom={eventRoom} eventLecturers={eventLecturers} eventNote={eventNote} eventSlot={eventSlot} />;
             } else {
-                return null;
+                return null
             }
         }
     }