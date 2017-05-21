7fcc467d194044797a182d0a155a9557d7693b53
loehnertz <jakob2102@gmail.com>
2017-05-21 12:46:45 +0200

    The 'flex' properties in the 'EventItem' component are now changing according to the 'orientation' (this caused visual bugs beforehand)

--- src/components/EventItemComponent.js
+++ src/components/EventItemComponent.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import {
     Text,
     TouchableWithoutFeedback,
-    Image,
-    Platform
+    Image
 } from 'react-native';
+import { connect } from 'react-redux';
 import { Card, CardSection } from './common';
 
 class EventItem extends Component {
@@ -20,37 +20,46 @@ class EventItem extends Component {
         return lecturersString;
     }
 
+    titleTextFlex() {
+        // TODO: Sometimes this is not working especially when going from 'lanscape' to 'portrait' (see Issue #28)
+        if (this.props.orientation === 'PORTRAIT') {
+            return ({ flexGrow: -1, flex: 12 });
+        } else if (this.props.orientation === 'LANDSCAPE') {
+            return ({ flex: -1, flexGrow: 12 });
+        }
+    }
+
     render() {
         return (
             <TouchableWithoutFeedback>
                 <Card>
                     <CardSection>
                         <Image source={require('../assets/images/clock.png')} style={styles.imageTitle} />
-                        <Text style={styles.titleText}>
+                        <Text style={[styles.titleText, this.titleTextFlex()]}>
                             {this.props.eventSlot}
                         </Text>
                     </CardSection>
                     <CardSection>
                         <Image source={require('../assets/images/subject.png')} style={styles.imageTitle} />
-                        <Text style={styles.titleText}>
+                        <Text style={[styles.titleText, this.titleTextFlex()]}>
                             {this.props.eventName}
                         </Text>
                     </CardSection>
                     <CardSection>
                         <Image source={require('../assets/images/house.png')} style={styles.imageTitle} />
-                        <Text style={styles.titleText}>
+                        <Text style={[styles.titleText, this.titleTextFlex()]}>
                             {this.props.eventRoom}
                         </Text>
                     </CardSection>
                     <CardSection>
                         <Image source={require('../assets/images/person.png')} style={styles.imageTitle} />
-                        <Text style={styles.titleText}>
+                        <Text style={[styles.titleText, this.titleTextFlex()]}>
                             {this.renderLecturers()}
                         </Text>
                     </CardSection>
                     <CardSection>
                         <Image source={require('../assets/images/note.png')} style={styles.imageTitle} />
-                        <Text style={styles.titleText}>
+                        <Text style={[styles.titleText, this.titleTextFlex()]}>
                             {this.props.eventNote}
                         </Text>
                     </CardSection>
@@ -66,15 +75,19 @@ const styles = {
         flexDirection: "column"
     },
     titleText: {
-        fontSize: 19,
-        flex: 12  // needs 'flexGrow' here for the iOS version to display it correctly in the 'WeekView', that breaks the 'DayView' though! sTODO: Fix this
+        fontSize: 19
     },
     imageTitle: {
         height: 21,
         width: 21,
-        //flex: 1,
         marginRight: 11
     }
 };
 
-export default EventItem;
+const mapStateToProps = state => {
+    return {
+        orientation: state.timetable.orientation
+    }
+};
+
+export default connect(mapStateToProps, {})(EventItem);
