bcc89102338101a3090703632ee10df353ac0efa
loehnertz <jakob2102@gmail.com>
2017-06-17 01:08:21 +0200

    Added (so far broken) iOS support (not compiling at the moment)

--- ios/HSRMMedialabTimetableApp/AppDelegate.m
+++ ios/HSRMMedialabTimetableApp/AppDelegate.m
@@ -11,9 +11,12 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-#import "../../node_modules/react-native-orientation/iOS/RCTOrientation/Orientation.h"  // TODO: Change this to a proper import when possible
+#import "../../node_modules/react-native-orientation/iOS/RCTOrientation/Orientation.h"
+#import "../../node_modules/react-native-onesignal/ios/RCTOneSignal/RCTOneSignal.h"
+// TODO: Change this to a proper import when possible
 
 @implementation AppDelegate
+@synthesize oneSignal = _oneSignal;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
@@ -26,17 +29,27 @@
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
+  
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
+  // For requiring push notification permissions manually.
+  self.oneSignal = [[RCTOneSignal alloc] initWithLaunchOptions:launchOptions
+                                                         appId:@"58a6a092-c7c6-44ca-bc38-f8c0b152e1f3"
+                                                      settings:@{kOSSettingsKeyAutoPrompt: @false}];
+  
   return YES;
 }
 
 - (UIInterfaceOrientationMask)application:(UIApplication *)application supportedInterfaceOrientationsForWindow:(UIWindow *)window {
   return [Orientation getOrientation];
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)notification {
+  [RCTOneSignal didReceiveRemoteNotification:notification];
 }
 
 @end
